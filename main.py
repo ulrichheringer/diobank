3f64a74edbd3264e445e53fcadcfae9c6b7de5c3
Pedro Ulrich <heringer.ulrich@gmail.com>
2024-08-27 20:35:20 -0300

    feat: Função de saque implementada

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ LIMITE_SAQUES = 3
 
 # Operacoes permitidas
 operacoes = """
-    [1] - Deposito
-    [2] - Saque
-    [3] - Extrato
-    [4] - Sair
-"""
+[1] - Deposito
+[2] - Saque
+[3] - Extrato
+[4] - Sair
+Digite uma opcao: """
 
 saldo = 0
 # Array de Dicionarios, para guardar o extrato
@@ -21,15 +21,19 @@ while True:
         case "1":
             while True:
                 valor = float(
-                    input("Por favor digite um valor para o depósito:"))
+                    input("Por favor digite um valor para o depósito: "))
                 if valor < 0:
                     print("Por favor, digite um valor maior do que R$0")
                     continue
+                else:
+                    extrato.append({"op": "D", "v": valor})
+                    print(
+                        f"Depósito de {valor:.2f} adicionado com sucesso a sua conta!")
         case "2":
             pass
         case "3":
             pass
         case "4":
-            pass
+            break
         case _:
             continue
